b4bc0824f161d4ff2cf09548b2e3fe86132d9c27
juntaki <me@juntaki.com>
2016-04-30 22:43:38 +0900

    Remove emacs.d

--- .emacs.d/inits/30-ruby.el
+++
@@ -1,4 +0,0 @@
-(autoload 'enh-ruby-mode "enh-ruby-mode"
-  "Mode for editing ruby source files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$latex " . enh-ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . enh-ruby-mode))
--- .emacs.d/package.sh
+++
@@ -1,1 +0,0 @@
-apt-get install llvm libclang-dev texinfo
