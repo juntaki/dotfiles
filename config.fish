b7eafad6eb39d0fae1cce0ccc8de6c4de7382781
juntaki <me@juntaki.com>
2016-09-14 20:49:55 +0900

    Fix PATH

--- config.fish
+++ config.fish
@@ -11,13 +11,15 @@ set -x TZ 'Asia/Tokyo'
 # rbenv
 set -x RBENV_ROOT $HOME/.rbenv
 set -x PATH $RBENV_ROOT/bin $PATH
-rbenv rehash
+set -x PATH $RBENV_ROOT/shims $PATH
+rbenv rehash >/dev/null ^&1
 
 # pyenv
 set -x PYENV_ROOT $HOME/.pyenv
 set -x PATH $PYENV_ROOT/bin $PATH
+set -x PATH $PYENV_ROOT/shims $PATH
 set -x PATH $PYENV_ROOT/versions/anaconda3-4.1.0/bin/ $PATH
-pyenv rehash
+pyenv rehash >/dev/null ^&1
 
 # golang
 set -x GOPATH $HOME/.go
